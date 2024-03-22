04ab722b9c99c037930ed981bd164582b36c3e95
Mostafa Mohamed Mostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2024-03-22 17:39:35 +0200

    adding navigation

--- src/Component/Change.js
+++ src/Component/Change.js
@@ -1,15 +1,24 @@
-<div className="card"  >
-         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <ul className="list-group list-group-flush">
-            <li className="list-group-item">Moustafa Mohamed</li>
-            <li className="list-group-item">Phone Number </li>
-            <li className="list-group-item">Email </li>
-            <li className="list-group-item">GitHub </li>
- 
-        </ul>
-   
-</div>
-
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
